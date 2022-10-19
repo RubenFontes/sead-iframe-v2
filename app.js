17d72d0491034d1e79bd5c11b1b60b308bd59238
Ruben Fontes <55508678+RubenFontes@users.noreply.github.com>
2022-10-19 15:49:13 -0300

    add app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+$(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: false,
+    //nav:true,
+    //rtl:false,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    items: 4,
+    /*responsive: {
+      0:{
+        items:1,
+        nav: false
+      },
+      600:{
+        items:2,
+        nav: false
+      },
+      1000:{
+        items:4,
+        nav: false
+      }
+    }*/
+  });
