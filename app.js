7e40bdc540e3f2813044baf0a43e60a1cfd789a6
Ruben Fontes <55508678+RubenFontes@users.noreply.github.com>
2022-10-24 11:50:07 -0300

    update app.js

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ $(".carousel").owlCarousel({
     autoplayHoverPause: true,
     items: 3,
     responsive: {
-      0:{
+      100:{
         items:1,
         nav: false
-      }
+      },
       /*600:{
         items:2,
         nav: false
-      },
-      1000:{
-        items:4,
-        nav: false
       }*/
+      2000:{
+        items:3,
+        nav: false
+      }
     }
   });
