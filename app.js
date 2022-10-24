49315b80d8ee36d4ed977d015a1564592410bd0f
Ruben Fontes <55508678+RubenFontes@users.noreply.github.com>
2022-10-24 12:02:51 -0300

    fix responsive

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ $(".carousel").owlCarousel({
     items: 3,
     responsive: {
       0:{
-        items:1,
+        items: 2,
         nav: false
       },
       600:{
-        items:3,
+        items: 3,
         nav: false
       },
       1000:{
-        items:4,
+        items: 4,
         nav: false
       }
     }
